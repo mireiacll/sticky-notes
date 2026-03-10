fb14c08846fe6e580649988e3071ae5274bdb9c5
Mireia <mireia@gaia3d.com>
2026-03-10 15:28:16 +0900

    delete button added

--- javascript.js
+++ javascript.js
@@ -2,8 +2,16 @@ const addBtn = document.getElementById("addNoteBtn");
 const board = document.getElementById("board");
 
 addBtn.addEventListener("click", () => {
+    // Create a new note element
+
     const note = document.createElement("div");
     note.classList.add("note");
-    note.innerHTML = '<textarea placeholder="Write something..."></textarea>';
+    note.innerHTML = '<button class="deleteBtn">X</button><textarea placeholder="Write something..."></textarea>'; 
     board.appendChild(note);
+
+    // Add event listener to the delete button of the new note
+    const deleteBtn = note.querySelector(".deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+        note.remove();
+    });
 });
