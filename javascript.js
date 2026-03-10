ccd31087a9cdf34f8393b678663787e2ad727a25
Mireia <mireia@gaia3d.com>
2026-03-10 15:47:55 +0900

    make the note draggable
    * to make it draggable need to add event listeners for mousedown, mousemove and mouseup
    * disable flexbox for the board and use absolute positioning for the notes

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ addBtn.addEventListener("click", () => {
     const note = document.createElement("div");
     note.classList.add("note");
     note.innerHTML = '<button class="deleteBtn">X</button><textarea placeholder="Write something..."></textarea>'; 
+    note.style.left ="50px";
+    note.style.top ="50px";
     board.appendChild(note);
 
     // Add event listener to the delete button of the new note
@@ -14,4 +16,22 @@ addBtn.addEventListener("click", () => {
     deleteBtn.addEventListener("click", () => {
         note.remove();
     });
+
+    makeDraggable(note);
 });
+
+function makeDraggable(note) {
+    let offsetX, offsetY;  
+    note.addEventListener("mousedown", (e) => {
+        offsetX = e.clientX - note.offsetLeft;
+        offsetY = e.clientY - note.offsetTop;
+        function moveNote(e) {
+            note.style.left = (e.clientX - offsetX) + "px";
+            note.style.top = (e.clientY - offsetY) + "px";
+        }
+        document.addEventListener("mousemove", moveNote);
+        document.addEventListener("mouseup", () => {
+            document.removeEventListener("mousemove", moveNote);
+        },{once:true});    
+    });
+}
